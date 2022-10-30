80c23be2dfb66d2d73eeab1ae18882e6cd60c2b0
SC-Dixon <shantay1234.sd@gmail.com>
2022-10-30 18:05:08 -0500

    Updated styles.css file and Added javascript functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,54 @@
 /* Add your JavaScript to this file */
+window.onload - function()
+{
+    var myform = document.querySelector('form');
+
+    myform.onsubmit = function(e){
+
+        e.preventDefault();
+
+        var email = document.querySelector('#email');
+
+        clearMessage();
+
+
+        if (email.value.trim() === "") {
+            showError("email-error", "Please enter a valid email address.")
+            e.preventDefault();
+            return false;
+        };
+
+        document.querySelector(".message").classList.add("email-success");
+        document.querySelector(".message").innerHTML = 'Thank you! Your email address ${myform.email.value} has been added to our mailing list!';
+
+        
+        return false;
+
+        
+    };
+
+
+
+function clearMessage() {
+    var elementsWithMessage = document.querySelectorAll('.message');
+    //console.log(elementsWithErrors);
+    for (var x = 0; x < elementsWithErrors.length; x++) 
+    {
+      elementsWithMessage[x].setAttribute('class', '');
+      elementsWithMessage[x].parentNode.removeChild(elementsWithMessage[x].nextElementSibling);
+      //console.log(elementsWithErrors[x].nextElementSibling);
+    }
+}
+
+
+
+    function showError(errorElement, errorMessage){
+        document.querySelector(".message"+errorElement).classList.add("email-error");
+        document.querySelector(".message"+errorElement).innerHTML = errorMessage;
+    }
+
+};
+
+
+
+  
